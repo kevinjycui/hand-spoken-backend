78bed9866068baa489f276570bb14d7a643f88d7
Aphexis <stephxca@gmail.com>
2019-09-08 07:43:51 -0400

    fixes

--- data_processor.py
+++ data_processor.py
@@ -5,9 +5,10 @@ def setGlyphs(src='test_glyphs'):
     with open('font.json') as json_file:
         data = json.load(json_file)
     i = 0
+    val=['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     for key in data['glyphs'].keys():
-        if os.path.exists('my_fonts/' + src + '/'+str(i)+'.svg') and os.path.isfile('my_fonts/' + src + '/'+str(i)+'.svg'):
-            data['glyphs'][key]['src'] = 'my_fonts/' + src + '/'+str(i)+'.svg'
+        if os.path.exists('my_fonts/' + src + '/'+val[i]+'.svg') and os.path.isfile('my_fonts/' + src + '/'+val[i]+'.svg'):
+            data['glyphs'][key]['src'] = 'my_fonts/' + src + '/'+val[i]+'.svg'
         else:
             data['glyphs'][key]['src'] = 'my_fonts/default.svg'
         i += 1
